fecceb1c954ae0aeb5f05df916add6a804464494
victor-develop <victor.develop@outlook.com>
2017-12-14 05:07:07 +0800

    update: break files, too large to view

--- solution/client/public/bundle.js
+++ solution/client/public/bundle.js
@@ -2,136 +2,25 @@
   
     document.addEventListener("DOMContentLoaded", ready);
   
-    const logger = {
-      info: console.log,
-      err: console.log
-    };
-  
-    const CellState = {
-      ActiveStill: 'ActiveStill',
-      AliveFromDeath: 'AliveFromDeath'
-    };
-  
-    // "Class" definitions
-    const Cell = (x, y, uid, state, overlayColor) => { 
-      return { x, y, uid, state, overlayColor } 
-    };
-  
-    const Board = (width, height, cells) => {
-  
-      const hashCells = {};
-  
-      try {
-        cells.forEach(cell => {
-          hashCells[cell.x] = hashCells[cell.x] || {};
-          hashCells[cell.x][cell.y] = cell;
-        });
-      }
-      catch(e) {
-        logger.err(e);
-        throw e;
-      }
-  
-      return {
-        width,
-        height,
-        cells: hashCells
-      }
-    };
-  
-    const Player = (uid, color, name) => {
-      return {
-        uid,
-        color,
-        name
-      }
-    }
-  
-    // sample data models
-    const players = [
-      Player('aaa', '#226ad6', 'Tom'),
-      Player('bbb', '#f20cdb', 'Cat'),
-      Player('ccc', '#0d6d06', 'Fok')
-    ]
-  
-    const currentUser = players[0];
-    
-    const game = {
-      board: Board(20, 20, [
-        Cell(0, 1, 'aaa', CellState.ActiveStill, 'red'),
-        Cell(1, 1, 'aaa', CellState.ActiveStill,'blue'),
-        Cell(2, 1, 'aaa', CellState.AliveFromDeath, 'green')
-      ])
-    }
-
-    const tick2 = {
-      board: Board(20, 20, [
-        Cell(1, 0, 'aaa', CellState.ActiveStill, 'red'),
-        Cell(1, 1, 'aaa', CellState.ActiveStill,'blue'),
-        Cell(1, 2, 'aaa', CellState.AliveFromDeath, 'green')
-      ])
-    }
-  
     function ready() {
       const Vue = window.Vue;
 
-      Vue.component('player-banner', {
-        template:`<span>
-                    {{ player.name }}
-                    <span class="player-color" v-bind:style="styleObject"></span>
-                  </span>
-                 `,
-        props: ['player'],
-        computed: {
-          styleObject: function() {
-            return {
-              backgroundColor: this.player.color
-            }
-          }
-        }
-      });
-  
-      Vue.component('cell', {
-        template: `<span class="cell" v-bind:style="styleObject"></span>`,
-        props: ['x', 'y', 'board'],
-        computed: {
-          styleObject: function() {
-            const cells = this.board.cells;
-            if(!(cells[this.x] && cells[this.x][this.y])) {
-              return {};
-            }
-            const cell = cells[this.x][this.y];
-            return {
-              backgroundColor: cell.overlayColor
-            };
-          }
-        }
-      })
+      const game = window.game;
 
-      // TODO: feature: explore borderless board
-      Vue.component('game-board', {
-        template: `<table class="board">
-                    <tbody>
-                      <tr v-for="h in board.height">
-                        <td v-for="w in 60">
-                          <cell :x=w-1 :y=h-1 :board=board></cell>
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
-                `,
-        props: ['board']
-      });
+      const players = window.players;
+
+      const tick2 = window.tick2;
     
+      const currentPlayer = window.currentPlayer;
+
       const app = new Vue({
         el: '#app',
         data: {
           game,
+          currentPlayer,
           players
         }
       });
-
-
 
       setInterval(() => {
         if (app.game == game) {
