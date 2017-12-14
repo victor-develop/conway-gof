0d7349359a1fcfd73d2909589663bb9629fb1bda
victor-develop <victor.develop@outlook.com>
2017-12-14 18:33:50 +0800

    refactor: move common/* to common/src and mkdir a common/tests

--- solution/ts_code/common/gamemodels/board.ts
+++
@@ -1,7 +0,0 @@
-import ICell from './icell'
-
-export default class Board {
-  width: number
-  height: number
-  cells: ICell[]
-}
---
+++ solution/ts_code/common/src/gamemodels/board.ts
@@ -0,0 +1,49 @@
+import ICell from './icell'
+import IPos from './ipos'
+import ILogger from '../ilogger'
+
+interface HashCells {
+  [x: number]: {
+    [y: number]: Object,
+  }
+}
+
+export class BoardBuilder {
+
+  private logger: ILogger
+
+  public Create(w: number, h: number, positions: IPos[]): Board {
+    return new Board(w, h, positions, this.logger)
+  }
+}
+
+export class Board {
+
+  constructor(w: number, h: number, positions: IPos[], aLogger: ILogger) {
+    this.width = w
+    this.height = h
+    this.cells = {}
+
+    this.listcells = positions
+    this.logger = aLogger
+    this.init()
+  }
+
+  private init() {
+    const hashCells = this.cells
+    this.listcells.forEach((pos) => {
+      hashCells[pos.x] = hashCells[pos.x] || {}
+      hashCells[pos.x][pos.y] = pos
+    })
+  }
+
+  public width: number
+  public height: number
+  public cells: HashCells
+
+  /**
+   * Actual cells
+   */
+  private listcells: IPos[]
+  private logger: ILogger
+}
