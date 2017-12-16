28443fe6a03273b7844aa4afd85f991e4f2675c4
victor-develop <victor.develop@outlook.com>
2017-12-16 11:13:43 +0800

    add: basic baord test

--- solution/ts_code/common/tests/gamemodels/board.spec.ts
+++ solution/ts_code/common/tests/gamemodels/board.spec.ts
@@ -6,13 +6,26 @@ import { presets } from '../../../common/src/gamemodels/preset-pattern';
 
 const testLogger = new TempLogger('Board test')
 
-describe('Board class', () => {
+describe('Board.create method', () => {
   const width = 10
   const height = 10
   const positions = presets[0]
-  it('create() will return a new Board instance', (done) => {
-    const board = Board.create(testLogger)(width, height, positions)
+  const board = Board.create(testLogger)(width, height, positions)
+  it('will return a new Board instance', (done) => {
     assert.ok(board)
+    done()
+  })
+  it('has correct height and width', (done) => {
+    assert.equal(board.width, width)
+    assert.equal(board.height, height)
+    done()
+  })
+  it('has a correct hash cell structure', (done) => {
+    assert.deepEqual(board.cells, {
+      0: { 1: { x: 0, y: 1 } },
+      1: { 1: { x: 1, y: 1 } },
+      2: { 1: { x: 2, y: 1 } },
+    })
     done()
   })
 })
