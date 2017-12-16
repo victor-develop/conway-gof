812c72beaf3431ca6ec2bcc670214ec2ad7d4543
victor-develop <victor.develop@outlook.com>
2017-12-16 21:35:53 +0800

    cont: previous commit

--- solution/ts_code/client/tests/event-emit.spec.ts
+++ solution/ts_code/client/tests/event-emit.spec.ts
@@ -1,13 +1,14 @@
 import 'mocha'
 import * as assert from 'assert'
-import * as EventEmitter from 'eventemitter3'
+import { EventEmitter } from 'eventemitter3'
+import { createEventBus } from '../src/event-bus'
 
 /**
  * Why am I testing a 3rd party lib? Just see if I use it correctly, serves like sample code
  */
 describe('EventBus events will be bound and emitted correctly', () => {
   it('should fire hello event', (done) => {
-    const bus = new EventEmitter.EventEmitter()
+    const bus = createEventBus()
     const hello = 'hello'
     let fired = false
     bus.on(hello, () => {
