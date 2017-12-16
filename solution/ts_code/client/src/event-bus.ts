d38bbe2de585e7a8555a6855910c60ab4cf67a99
victor-develop <victor.develop@outlook.com>
2017-12-16 21:35:13 +0800

    add: EventEmitter wrapper

--- solution/ts_code/client/src/event-bus.ts
+++ solution/ts_code/client/src/event-bus.ts
@@ -1,8 +1,6 @@
 import { EventEmitter } from 'eventemitter3'
 import { IEventBus } from './ievent-bus'
 
-export default class EventBus
-  extends EventEmitter
-  implements IEventBus {
-
-  }
+export function createEventBus(): IEventBus {
+  return new EventEmitter()
+}
