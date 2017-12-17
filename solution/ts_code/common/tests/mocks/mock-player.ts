a40faf80686d2f754b54af7e6d78c08a5203d2e8
victor-develop <victor.develop@outlook.com>
2017-12-17 22:34:38 +0800

    update: change player obj to player create function

--- solution/ts_code/common/tests/mocks/mock-player.ts
+++ solution/ts_code/common/tests/mocks/mock-player.ts
@@ -2,7 +2,7 @@
 import { Color } from '../../src/gamemodels/color'
 import IPlayer from '../../src/gamemodels/iplayer'
 
-export default function createPlayer(uid: string, name: string, color: Color): IPlayer {
+export default function createPlayer(name: string, color: string, uid: Color): IPlayer {
   return {
     uid,
     name,
